68229cab8a0a00f8a0fa0b4bb1eb0bd899d0decb
mitchellsl <mitch@sociallane.nl>
2026-01-03 16:59:33 +0100

    refactor: update image slider widget to use `get_safe_setting` for all settings access

--- widgets/image-slider/includes/render/render-widget.php
+++ widgets/image-slider/includes/render/render-widget.php
@@ -40,16 +40,16 @@ $slider_template = isset($settings['slider_template']) ? $settings['slider_templ
 $slider_settings = [
     'slidesPerView' => isset($settings['slides_per_view']) ? $settings['slides_per_view'] : 1,
     'spaceBetween' => isset($settings['space_between']['size']) ? intval($settings['space_between']['size']) : 30,
-    'navigation' => $settings['slider_navigation'] === 'yes',
-    'pagination' => $settings['slider_pagination'] === 'yes',
-    'loop' => $settings['slider_loop'] === 'yes',
+    'navigation' => $this->get_safe_setting('slider_navigation', '') === 'yes',
+    'pagination' => $this->get_safe_setting('slider_pagination', '') === 'yes',
+    'loop' => $this->get_safe_setting('slider_loop', '') === 'yes',
     'effect' => isset($settings['slider_effect']) ? $settings['slider_effect'] : 'slide',
     'autoplay' => $settings['slider_autoplay'] === 'yes' ? [
         'delay' => isset($settings['slider_autoplay_delay']) ? intval($settings['slider_autoplay_delay']) : 5000,
         'disableOnInteraction' => false,
     ] : false,
     'speed' => isset($settings['slider_speed']) ? intval($settings['slider_speed']) : 500,
-    'centeredSlides' => $settings['centered_slides'] === 'yes',
+    'centeredSlides' => $this->get_safe_setting('centered_slides', '') === 'yes',
     'breakpoints' => [
         // Mobile (0px and up)
         0 => [
@@ -89,7 +89,7 @@ $gradient_right_style = "background: linear-gradient(to left, {$rgba_start} 0%, 
 ?>
 
 <div class="promen-image-slider-widget">
-    <?php if ($settings['show_title'] === 'yes') : ?>
+    <?php if ($this->get_safe_setting('show_title', '') === 'yes') : ?>
         <div class="promen-slider-title-wrapper">
             <?php echo promen_render_split_title($this, $settings, 'section_title', 'promen-slider'); ?>
         </div>
@@ -139,22 +139,22 @@ $gradient_right_style = "background: linear-gradient(to left, {$rgba_start} 0%, 
                         <!-- Slides -->
                         <div class="swiper-slide">
                             <div class="promen-slider-image-wrapper">
-                                <div class="promen-slider-image elementor-animation-<?php echo esc_attr($settings['image_hover_animation']); ?>">
+                                <div class="promen-slider-image elementor-animation-<?php echo esc_attr($this->get_safe_setting('image_hover_animation', '')); ?>">
                                     <?php if (!empty($item['image']['url'])) : ?>
                                         <?php 
                                         // Apply the appropriate height and object-fit settings
                                         $image_classes = 'promen-slider-img';
                                         
                                         // Apply aspect ratio container class if needed
-                                        if ($settings['image_height'] === 'aspect_ratio') {
+                                        if ($this->get_safe_setting('image_height', 'default') === 'aspect_ratio') {
                                             $image_classes .= ' promen-aspect-ratio-img';
                                             // Add aspect-ratio class to the container
                                             echo '<div class="aspect-ratio">';
                                         }
                                         
                                         // Add object-fit class if specified
-                                        if ($settings['image_height'] !== 'default' && !empty($settings['image_object_fit'])) {
-                                            $image_classes .= ' object-fit-' . esc_attr($settings['image_object_fit']);
+                                        if ($this->get_safe_setting('image_height', 'default') !== 'default' && !empty($this->get_safe_setting('image_object_fit', ''))) {
+                                            $image_classes .= ' object-fit-' . esc_attr($this->get_safe_setting('image_object_fit', ''));
                                         }
 
                                         // Get image metadata if using image metadata options
@@ -185,19 +185,19 @@ $gradient_right_style = "background: linear-gradient(to left, {$rgba_start} 0%, 
                                         <img src="<?php echo esc_url($item['image']['url']); ?>" 
                                              alt="<?php echo esc_attr($image_title); ?>" 
                                              class="<?php echo esc_attr($image_classes); ?>">
-                                        <?php if ($settings['image_height'] === 'aspect_ratio') : ?>
+                                        <?php if ($this->get_safe_setting('image_height', 'default') === 'aspect_ratio') : ?>
                                             </div>
                                         <?php endif; ?>
                                     <?php endif; ?>
                                     
-                                    <?php if (($settings['show_image_title'] === 'yes' || $settings['show_image_description'] === 'yes') && 
-                                              ($item['show_overlay'] === 'yes' || $settings['show_overlay_on_hover'] === 'yes')) : ?>
-                                        <div class="promen-slider-overlay<?php echo $settings['show_overlay_on_hover'] === 'yes' ? ' hover-overlay' : ''; ?>">
-                                            <?php if ($settings['show_image_title'] === 'yes' && !empty($image_title)) : ?>
+                                    <?php if (($this->get_safe_setting('show_image_title', '') === 'yes' || $this->get_safe_setting('show_image_description', '') === 'yes') && 
+                                              ($item['show_overlay'] === 'yes' || $this->get_safe_setting('show_overlay_on_hover', '') === 'yes')) : ?>
+                                        <div class="promen-slider-overlay<?php echo $this->get_safe_setting('show_overlay_on_hover', '') === 'yes' ? ' hover-overlay' : ''; ?>">
+                                            <?php if ($this->get_safe_setting('show_image_title', '') === 'yes' && !empty($image_title)) : ?>
                                                 <p class="promen-slider-overlay-title"><?php echo esc_html($image_title); ?></p>
                                             <?php endif; ?>
                                             
-                                            <?php if ($settings['show_image_description'] === 'yes' && !empty($image_description)) : ?>
+                                            <?php if ($this->get_safe_setting('show_image_description', '') === 'yes' && !empty($image_description)) : ?>
                                                 <p class="promen-slider-overlay-description"><?php echo esc_html($image_description); ?></p>
                                             <?php endif; ?>
                                         </div>
@@ -225,22 +225,22 @@ $gradient_right_style = "background: linear-gradient(to left, {$rgba_start} 0%, 
         <div class="<?php echo esc_attr($grid_classes); ?>">
             <?php foreach ($slider_images as $index => $item) : ?>
                 <div class="promen-slider-image-wrapper">
-                    <div class="promen-slider-image elementor-animation-<?php echo esc_attr($settings['image_hover_animation']); ?>">
+                    <div class="promen-slider-image elementor-animation-<?php echo esc_attr($this->get_safe_setting('image_hover_animation', '')); ?>">
                         <?php if (!empty($item['image']['url'])) : ?>
                             <?php 
                             // Apply the appropriate height and object-fit settings
                             $image_classes = 'promen-slider-img';
                             
                             // Apply aspect ratio container class if needed
-                            if ($settings['image_height'] === 'aspect_ratio') {
+                            if ($this->get_safe_setting('image_height', 'default') === 'aspect_ratio') {
                                 $image_classes .= ' promen-aspect-ratio-img';
                                 // Add aspect-ratio class to the container
                                 echo '<div class="aspect-ratio">';
                             }
                             
                             // Add object-fit class if specified
-                            if ($settings['image_height'] !== 'default' && !empty($settings['image_object_fit'])) {
-                                $image_classes .= ' object-fit-' . esc_attr($settings['image_object_fit']);
+                            if ($this->get_safe_setting('image_height', 'default') !== 'default' && !empty($this->get_safe_setting('image_object_fit', ''))) {
+                                $image_classes .= ' object-fit-' . esc_attr($this->get_safe_setting('image_object_fit', ''));
                             }
 
                             // Get image metadata if using image metadata options
@@ -271,19 +271,19 @@ $gradient_right_style = "background: linear-gradient(to left, {$rgba_start} 0%, 
                             <img src="<?php echo esc_url($item['image']['url']); ?>" 
                                  alt="<?php echo esc_attr($image_title); ?>" 
                                  class="<?php echo esc_attr($image_classes); ?>">
-                            <?php if ($settings['image_height'] === 'aspect_ratio') : ?>
+                            <?php if ($this->get_safe_setting('image_height', 'default') === 'aspect_ratio') : ?>
                                 </div>
                             <?php endif; ?>
                         <?php endif; ?>
                         
-                        <?php if (($settings['show_image_title'] === 'yes' || $settings['show_image_description'] === 'yes') && 
-                                  ($item['show_overlay'] === 'yes' || $settings['show_overlay_on_hover'] === 'yes')) : ?>
-                            <div class="promen-slider-overlay<?php echo $settings['show_overlay_on_hover'] === 'yes' ? ' hover-overlay' : ''; ?>">
-                                <?php if ($settings['show_image_title'] === 'yes' && !empty($image_title)) : ?>
+                        <?php if (($this->get_safe_setting('show_image_title', '') === 'yes' || $this->get_safe_setting('show_image_description', '') === 'yes') && 
+                                  ($item['show_overlay'] === 'yes' || $this->get_safe_setting('show_overlay_on_hover', '') === 'yes')) : ?>
+                            <div class="promen-slider-overlay<?php echo $this->get_safe_setting('show_overlay_on_hover', '') === 'yes' ? ' hover-overlay' : ''; ?>">
+                                <?php if ($this->get_safe_setting('show_image_title', '') === 'yes' && !empty($image_title)) : ?>
                                     <p class="promen-slider-overlay-title"><?php echo esc_html($image_title); ?></p>
                                 <?php endif; ?>
                                 
-                                <?php if ($settings['show_image_description'] === 'yes' && !empty($image_description)) : ?>
+                                <?php if ($this->get_safe_setting('show_image_description', '') === 'yes' && !empty($image_description)) : ?>
                                     <p class="promen-slider-overlay-description"><?php echo esc_html($image_description); ?></p>
                                 <?php endif; ?>
                             </div>
