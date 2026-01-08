c679e64f0f020841768da50693b61942c03d1010
mitchellsl <mitch@sociallane.nl>
2026-01-08 09:14:49 +0100

    fix widget

--- widgets/locations-display/locations-display-widget.php
+++ widgets/locations-display/locations-display-widget.php
@@ -9,18 +9,22 @@ if (!defined('ABSPATH')) {
     exit; // Exit if accessed directly.
 }
 
-// Include asset registration file
-require_once(__DIR__ . '/locations-display-register.php');
-
 // Include the split title functions
 require_once(PROMEN_ELEMENTOR_WIDGETS_PATH . 'includes/controls/split-title-controls.php');
+
+// Include control files
+require_once(__DIR__ . '/includes/controls/content-controls.php');
+require_once(__DIR__ . '/includes/controls/style-controls.php');
+
+// Include render files
+require_once(__DIR__ . '/includes/render/render-functions.php');
 
 /**
  * Locations Display widget.
  *
  * Elementor widget that displays location information with images and addresses.
  */
-class Promen_Locations_Display_Widget extends \Elementor\Widget_Base {
+class Promen_Locations_Display_Widget extends \Promen_Widget_Base {
 
     /**
      * Get widget name.
@@ -68,14 +72,18 @@ class Promen_Locations_Display_Widget extends \Elementor\Widget_Base {
     }
 
     /**
-     * Enqueue widget styles and scripts
+     * Enqueue widget styles
+     *
+     * @return array Style dependencies
      */
     public function get_style_depends() {
-        return ['locations-display'];
+        return ['promen-locations-display-widget'];
     }
 
     /**
      * Enqueue widget scripts
+     *
+     * @return array Script dependencies
      */
     public function get_script_depends() {
         return ['gsap', 'gsap-scrolltrigger', 'promen-locations-display-accessibility'];
@@ -85,9 +93,11 @@ class Promen_Locations_Display_Widget extends \Elementor\Widget_Base {
      * Register widget controls.
      */
     protected function register_controls() {
-        // Include control files
-        require_once(__DIR__ . '/includes/content-controls.php');
-        require_once(__DIR__ . '/includes/style-controls.php');
+        // Register content controls
+        Promen_Locations_Display_Content_Controls::register_controls($this);
+        
+        // Register style controls
+        Promen_Locations_Display_Style_Controls::register_controls($this);
     }
 
     /**
@@ -95,8 +105,6 @@ class Promen_Locations_Display_Widget extends \Elementor\Widget_Base {
      */
     protected function render() {
         $settings = $this->get_settings_for_display();
-        
-        // Include the template file
-        require_once(__DIR__ . '/includes/template.php');
+        Promen_Locations_Display_Render::render_widget($this, $settings);
     }
-} 
+}
